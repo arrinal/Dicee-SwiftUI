219b1ef88e5f0b579ef28ce46ce692fba6ce9e75
Arrinal Sholifadliq <arrinalsf@gmail.com>
2020-05-03 15:41:47 +0700

    Commit 1

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Arrinal Sholifadliq on 30/04/20.
+//  Copyright © 2020 Arrinal Sholifadliq. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    dice(n: leftDiceNumber)
+                    dice(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                }
+                .padding(.horizontal)
+                .background(Color.red)
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct dice: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+        .padding()
+    }
+}
